e8015cde7d21865289a6e4ce4224cd1a53bbf298
Joshua Libradilla <joshlibra52tm@gmail.com>
2022-07-09 17:19:51 +0800

    Implemented Note and getNote

---
+++ lib/models/note.dart
@@ -0,0 +1,14 @@
+class Note {
+  String? noteID;
+  String? noteTile;
+  String? noteContent;
+  DateTime? createDateTime;
+  DateTime? lastEditDateTime;
+
+  Note(
+      {this.noteID,
+      this.noteTile,
+      this.noteContent,
+      this.createDateTime,
+      this.lastEditDateTime});
+}
--- lib/services/notes_services.dart
+++ lib/services/notes_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:consuming_rest_api/models/api_response.dart';
+import 'package:consuming_rest_api/models/note.dart';
 import 'package:consuming_rest_api/models/note_for_listing.dart';
 import 'package:http/http.dart' as http;
 
@@ -30,5 +31,26 @@ class NotesSerivice {
           error: true, errorMessage: 'An error occurs');
     }).catchError((_) => APIResponse<List<NoteForListing>>(
         error: true, errorMessage: 'An error occurs'));
+  }
+
+  Future<APIResponse<Note>> getNote(String noteID) {
+    return http.get(Uri.parse(api + '/notes/' + noteID), headers: headers).then(
+        (data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        final note = Note(
+          noteID: jsonData['noteID'],
+          noteTile: jsonData['noteTitle'],
+          noteContent: jsonData['noteContent'],
+          createDateTime: DateTime.parse(jsonData['createDateTime']),
+          lastEditDateTime: jsonData['latestEditDateTime'] != null
+              ? DateTime.parse(jsonData['latestEditDateTime'])
+              : null,
+        );
+        return APIResponse<Note>(data: note);
+      }
+      return APIResponse<Note>(error: true, errorMessage: 'An error occured');
+    }).catchError(
+        (_) => APIResponse<Note>(error: true, errorMessage: 'An error occurs'));
   }
 }
