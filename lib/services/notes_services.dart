dbb5c95ee01410ff1a1e662dd15679db3b928eb6
Joshua Libradilla <joshlibra52tm@gmail.com>
2022-07-09 20:13:10 +0800

    Implemented Delete Method in Service

--- lib/services/notes_services.dart
+++ lib/services/notes_services.dart
@@ -66,4 +66,16 @@ class NotesSerivice {
     }).catchError((_) =>
             APIResponse<bool>(error: true, errorMessage: 'An error occurs'));
   }
+
+  Future<APIResponse<bool>> deleteNote(String noteID) {
+    return http
+        .delete(Uri.parse(api + '/notes/' + noteID), headers: headers)
+        .then((data) {
+      if (data.statusCode == 204) {
+        return APIResponse<bool>(data: true);
+      }
+      return APIResponse<bool>(error: true, errorMessage: 'An error occured');
+    }).catchError((_) =>
+            APIResponse<bool>(error: true, errorMessage: 'An error occurs'));
+  }
 }
