e43d66b03fc26ad4ff91a9d2aad9b2e9659466db
Joshua Libradilla <joshlibra52tm@gmail.com>
2022-07-09 13:09:27 +0800

    Created NoteDelete

--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -1,4 +1,5 @@
 import 'package:consuming_rest_api/models/note_for_listing.dart';
+import 'package:consuming_rest_api/views/note_delete.dart';
 import 'package:consuming_rest_api/views/note_modify.dart';
 import 'package:flutter/material.dart';
 
@@ -31,8 +32,8 @@ class NoteList extends StatelessWidget {
       appBar: AppBar(title: const Text('List of Notes')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => NoteModify()));
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const NoteModify()));
         },
         child: const Icon(Icons.add),
       ),
@@ -40,18 +41,37 @@ class NoteList extends StatelessWidget {
         separatorBuilder: (context, index) =>
             const Divider(height: 1, color: Colors.green),
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              notes[index].noteTile!,
-              style: TextStyle(color: Theme.of(context).primaryColor),
-            ),
-            subtitle: Text(
-                'Last edited on ${formatDateTime(notes[index].lastEditDateTime!)}'),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) =>
-                      NoteModify(noteID: notes[index].noteID)));
+          return Dismissible(
+            key: ValueKey(notes[index].noteID),
+            direction: DismissDirection.startToEnd,
+            onDismissed: (direction) {},
+            confirmDismiss: (direction) async {
+              final result = await showDialog(
+                  context: context, builder: (context) => const NoteDelete());
+              print(result);
+              return result;
             },
+            background: Container(
+              color: Colors.red,
+              padding: const EdgeInsets.only(left: 16),
+              child: const Align(
+                child: Icon(Icons.delete, color: Colors.white),
+                alignment: Alignment.centerLeft,
+              ),
+            ),
+            child: ListTile(
+              title: Text(
+                notes[index].noteTile!,
+                style: TextStyle(color: Theme.of(context).primaryColor),
+              ),
+              subtitle: Text(
+                  'Last edited on ${formatDateTime(notes[index].lastEditDateTime!)}'),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        NoteModify(noteID: notes[index].noteID)));
+              },
+            ),
           );
         },
         itemCount: notes.length,
